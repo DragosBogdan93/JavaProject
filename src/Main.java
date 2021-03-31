8ebc948540c9fc7e6c12f46617ce82e4a4c0e9bf
“DragosBogdan93 <dragosdumitras93@gmail.com>
2021-03-31 10:14:18 +0200

    Tema - Introducere in Java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,21 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world!");
+        System.out.println("Hello");
+        System.out.println("Dragos Bogdan Dumitras");
+        System.out.println(3+3);
+        float myFloat = 12312344.123F;
+        System.out.println(234/11f);
+        System.out.print("Rezultatul operatiei a. este:");
+        System.out.println(-5+8*6);
+        System.out.print("Rezultatul operatiei b. este:");
+        System.out.println((55+9)%9);
+        System.out.print("Rezultatul operatiei c. este:");
+        System.out.println(20+ -3*5/8f);
+        System.out.print("Rezultatul operatiei d. este:");
+        System.out.println(5+15/3*2-8%3);
+
+
+
     }
 }
